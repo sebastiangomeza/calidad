9a89ad864ccffe4b45bf30209536f0e1c27f109d
joan.gomez <joan.gomez@udea.edu.co>
2020-04-23 14:13:52 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,60 @@
+function seleccion() {
+    if (event.target.value == 3) {
+        console.log('3 lados');
+    }
+    else {
+        console.log('4 lados');
+    }
+}
+
+function validaNumericos() {
+    var inputtxt = event.target;
+    var valor = inputtxt.value;
+    for (i = 0; i < valor.length; i++) {
+        var code = valor.charCodeAt(i);
+        if (code <= 48 || code >= 57) {
+            inputtxt.value = "";
+            return;
+        }
+    }
+
+}
+function calcular() {
+    var lado1 = document.getElementById("value1").value;
+    var lado2 = document.getElementById("value2").value;
+    var lado3 = document.getElementById("value3").value;
+    var lado4 = document.getElementById("value4").value;
+    if (document.getElementById("lados").value == 3) {
+        tresLados(lado1, lado2, lado3)
+    } else {
+        cuatroLados(lado1, lado2, lado3, lado4)
+    }
+
+}
+
+function tresLados(lado1, lado2, lado3) {
+    if (lado1 == lado2 && lado2 == lado3) {
+        alert('Es un triangulo equilatero')
+        return
+    }
+    if (lado1 == lado2 || lado2 == lado3 || lado1 == lado3) {
+        alert('Es un triangulo Isoceles')
+        return
+    }
+    alert('Es un triangulo Escaleno')
+    return
+}
+
+function cuatroLados(lado1, lado2, lado3, lado4) {
+    if (lado1 == lado2 && lado2 == lado3 && lado3 == lado4) {
+        alert('Es un Cuadrado')
+        return
+    }
+    if (lado1 == lado3 && lado2 == lado4) {
+        alert('Es un Rectángulo')
+        return
+    }
+    alert('Es un Trapecio')
+
+    return
+}
