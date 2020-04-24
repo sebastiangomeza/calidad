27c51e7a1a6797fe7d2f049bdad2ff9fcf9e017e
sebastiangomeza <38189609+sebastiangomeza@users.noreply.github.com>
2020-04-24 10:02:11 -0500

    update script.js

--- script.js
+++ script.js
@@ -51,7 +51,7 @@ function calcular() {
             return;
         }
         if (lado1 == 0 || lado2 == 0 || lado3 == 0 || lado4 == 0) {
-            alert('por favor ingresar números enteros positivos mayores a cero')
+            alert('por favor ingresar números enteros mayores a cero')
             return;
         }
         cuatroLados(lado1, lado2, lado3, lado4)
